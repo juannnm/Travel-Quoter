3dc6c9d10fd5ad7721239a1fe075b7c6e50b8fbd
Juan <jcruz.munin@gmail.com>
2024-09-11 01:34:43 -0300

    Sidebar corrected

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,11 @@ import { LateralNavBar } from "./components/LateralNavBar";
 
 export const App = () => {
   return (
-    <div className="container-fluid justify-content-start m-0">
+    <div className="container-fluid  m-0">
       <div className="row">
-        <div className="col-2" style={{ backgroundColor: "#b1cbee" }}>
+
           <LateralNavBar />
-        </div>
+
         <div className="col justify-content-center">
           <FormContainer />
         </div>
